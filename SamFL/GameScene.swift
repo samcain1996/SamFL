9a66fa71c718a92b153db2e867b1da09c050d95c
Sam Cain <samuelrcain@gmail.com>
2023-11-10 14:02:02 -0600

    Removed unused code

--- SamFL/GameScene.swift
+++ SamFL/GameScene.swift
@@ -16,10 +16,8 @@ class GameScene: SKScene {
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var samFace : SKSpriteNode?
-    private var spinnyNode : SKShapeNode?
     
     private var initialized = false
-    private var isPlayingSong = false
     
     override func sceneDidLoad() {
 
@@ -42,52 +40,22 @@ class GameScene: SKScene {
             }
             
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//            
-//            //n.run(SKAction.playSoundFileNamed("SamFL.mp3", waitForCompletion: false))
-//        }
 
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        if (!self.initialized) { return }
+        if (!initialized) { return }
         
         if let samFace = self.samFace {
             if (!samFace.hasActions()) { samFace.run(SKAction.playSoundFileNamed("SamFL.mp3", waitForCompletion: true)) }
